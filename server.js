1d83a3d2f61831c3845c0f3f7d9b34cc929124c5
Titusz Wong <tituszwong@TituszsiMac24.local>
2025-03-23 16:37:09 +0800

    Add register and login event handlers

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ const users = {};
 
 io.on('connection', (socket) => {
     console.log('A user connected');
+
     socket.on('register', (data) => {
+        console.log('Register attempt:', data); // 測試用
         if (users[data.username]) {
             socket.emit('registerResponse', { message: '用戶名已存在' });
         } else {
@@ -24,7 +26,9 @@ io.on('connection', (socket) => {
             socket.emit('registerResponse', { message: '註冊成功，請登入' });
         }
     });
+
     socket.on('login', (data) => {
+        console.log('Login attempt:', data); // 測試用
         if (users[data.username] && users[data.username] === data.password) {
             socket.emit('loginResponse', { success: true, message: '登入成功' });
         } else {
